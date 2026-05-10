bd7275707e7ac92890290e1db722ccbd1dffdc6a
Christopher Cortez <chris06.cortez@gmail.com>
2026-05-10 13:45:55 +0100

    required for coursework

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,5 +1,11 @@
 const userDefinedTagCategories = {
 	"type": {
 		"description": "type of the paper"
+	},
+	"classification":{
+		"description" : "classification of paper"
+	},
+	"problem":{
+		"description" : "problem addressed by paper"
 	}
 }
